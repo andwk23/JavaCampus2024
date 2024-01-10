6bdf35c1e05d4fb7470db61499b12a0e3974639a
andeuw <66847734+andeuw@users.noreply.github.com>
2024-01-10 13:47:09 +0100

    Initial Commit local. Umgang mit Editor IntelliJ.

---
+++ _000_EditorTour/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int tst = 2;
+        int test2 = tst + 1 - 1;
+        System.out.println(tst + " " + test2);
+
+        ZweiteKlasseS zweiteClass = new ZweiteKlasseS();
+    }
+}
