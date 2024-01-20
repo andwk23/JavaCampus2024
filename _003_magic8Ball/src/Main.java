f19da3e4e4d6802dcfdbeb478434d289927ec5ab
andeuw <66847734+andeuw@users.noreply.github.com>
2024-01-20 17:37:14 +0100

    Ergänzungen, vorläufiges Endprodukt

--- _003_magic8Ball/src/Main.java
+++ _003_magic8Ball/src/Main.java
@@ -1,15 +1,26 @@
 import javax.swing.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args)
     {
         // Anzahl der Antwortmöglichkeiten einlesen
         int anzahl = leseAnzahl();
-        System.out.println(anzahl);
 
         //Antwortmöglichkeit einlesen
+        String[] antworten = leseAntworten(anzahl);
 
         // Frage einlesen
+        String antwort = "";
+        while (true){
+            String neueFrage = JOptionPane.showInputDialog(antwort + "Eine Frage stellen");
+            if(neueFrage.equals("")){
+                break;
+            }
+            else{
+                antwort = antworten[zufallsZahl(0,anzahl-1)] + " ";
+            }
+        }
 
         // Zufällige Antwort wählen
 
@@ -33,5 +44,25 @@ public class Main {
         }
 
         return anzahl;
+    }
+
+    public static String[] leseAntworten(int anzahl) {
+        String[] antworten = new String[anzahl];
+        int counter = 0;
+        try {
+            while (counter < anzahl){
+                antworten[counter] = JOptionPane.showInputDialog("Antwort Nummer:", counter);
+                counter++;
+            }
+        }
+        catch (Exception e){
+            System.out.println("Eingabe ist kein Antwort");
+        }
+    return antworten;
+    }
+
+    public static int zufallsZahl(int minimal, int maximal){
+        float number = (float) Math.random() * (maximal - minimal) + minimal;
+        return Math.round(number);
     }
 }
